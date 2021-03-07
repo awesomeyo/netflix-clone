ca553285038c248d0ef2ad4421136d77663b16c6
yeoyyl <yeoyyl92@gmail.com>
2021-03-07 23:14:05 +0800

    update row components for movie list

--- src/Row.js
+++ src/Row.js
@@ -1,25 +1,42 @@
-import React,{ useState, useEffect } from 'react'
-import axios from './axios'
-import requests from './requests'
+import React, { useState, useEffect } from "react";
+import axios from "./axios";
+import "./Row.css";
 
-export function Row({title, fetchUrl}) {
+const base_url = "https://image.tmdb.org/t/p/original/";
+
+export function Row({ title, fetchUrl, isLargeRow }) {
   const [movies, setMovies] = useState([]);
 
-  //A snippet of code which runs based on a specific condition 
+  //A snippet of code which runs based on a specific condition
 
-  useEffect(() =>{
-    async function fetchData(){
+  useEffect(() => {
+    async function fetchData() {
       const request = await axios.get(fetchUrl);
       setMovies(request.data.results);
       return request;
     }
-    fetchData()
-  }, [fetchUrl]) //if leave blank only run once, if put movies here, its gonna runs everytime movies changes
+    fetchData();
+  }, [fetchUrl]); //if leave blank only run once, if put movies here, its gonna runs everytime movies changes
 
-  console.log(movies)
+  console.table(movies);
+
   return (
     <>
       <h2>{title}</h2>
+      <div className="row__posters">
+        {movies.map((movie) => {
+          return (
+            <img
+              key={movie.id}
+              className={`row__poster ${isLargeRow && "row__posterLarge"}`}
+              src={`${base_url}${
+                isLargeRow ? movie.poster_path : movie.backdrop_path
+              }`}
+              alt={movie.name}
+            />
+          );
+        })}
+      </div>
     </>
-  )
+  );
 }
