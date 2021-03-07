4c6f7b1b3d1123b10dd38537aad1ecf0f5b483b3
yeoyyl <yeoyyl92@gmail.com>
2021-03-07 23:14:52 +0800

    update app file for diff movie category

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
-import './App.css';
-import { Row } from './Row'
-import requests from './requests'
-
+import "./App.css";
+import { Row } from "./Row";
+import requests from "./requests";
+import Banner from "./Banner";
 
 function App() {
   return (
-    <div className="App">
-      <Row title="NETFLIX ORIGINALS" fetchUrl={requests.fetchNetflixOriginals} />
-      <Row title="Trending Now" fetchUrl={requests.fetchTrending}/>
+    <div className="app">
+      <h1>Hey yoyo</h1>
+      <Row
+        title="NETFLIX ORIGINALS"
+        fetchUrl={requests.fetchNetflixOriginals}
+        isLargeRow={true}
+      />
+      <Row title="Trending Now" fetchUrl={requests.fetchTrending} />
+      <Row title="Top Rated" fetchUrl={requests.fetchTopRated} />
+      <Row title="Action Movies" fetchUrl={requests.fetcActionMovies} />
+      <Row title="Comedy Movies" fetchUrl={requests.fetchComedyMovies} />
+      <Row title="Horror Movies" fetchUrl={requests.fetchHorrorMovies} />
+      <Row title="Romance Movies" fetchUrl={requests.fetchRomanceMovies} />
+      <Row title="Documentaries" fetchUrl={requests.fetchDocumentaries} />
     </div>
   );
 }
